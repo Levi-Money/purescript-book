45118d5c52fbb6db6b13e3e5154840bd58445874
Walker <walker@praiseweb.com.br>
2021-06-20 18:23:58 -0300

    refact(ch3): add exercise 3 solution

--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,4 +2,20 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.AddressBook (AddressBook, Entry, findEntry)
+import Data.Maybe (Maybe)
+
 -- Note to reader: Add your solutions to this file
+
+findEntryByName :: String -> String -> AddressBook -> Maybe Entry
+findEntryByName firstName lastName = findEntry filter
+  where
+  filter :: Entry -> Boolean
+  filter entry = entry.firstName == firstName && entry.lastName == lastName
+
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = findEntry filter
+  where
+  filter :: Entry -> Boolean
+  filter = eq street <<< _.address.street
+
