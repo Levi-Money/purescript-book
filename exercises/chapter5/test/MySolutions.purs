afd21de33732d073eccf9677ae81cd1229ee09e8
Walker <walker@praiseweb.com.br>
2021-09-27 08:54:43 -0300

    feat(ch5): add binomial solution

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -5,3 +5,10 @@ import Prelude
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
+
+
+binomial :: Int -> Int -> Int
+binomial 0 _ = 0
+binomial n k | k > n = 0
+binomial n k = factorial n / ( factorial k * factorial (n - k) )
