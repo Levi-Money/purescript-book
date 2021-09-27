4eb647243c807402daffb35ad657d3adff5fc9e1
Walker <walker@praiseweb.com.br>
2021-09-27 09:15:40 -0300

    feat(ch5): add pascal solution

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -6,9 +6,11 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-
-
 binomial :: Int -> Int -> Int
 binomial 0 _ = 0
 binomial n k | k > n = 0
 binomial n k = factorial n / ( factorial k * factorial (n - k) )
+
+pascal :: Int -> Int -> Int
+pascal _ 0 = 1
+pascal n k = binomial (n - 1) (k - 1) + binomial (n - 1) k
