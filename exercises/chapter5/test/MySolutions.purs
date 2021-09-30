59a8c201e0b70bc229880b5a54f7e5fe51212c1b
Walker <walker@praiseweb.com.br>
2021-09-30 12:29:31 -0300

    feat(ch5): add shapeBounds solution

--- exercises/chapter5/test/MySolutions.purs
+++ exercises/chapter5/test/MySolutions.purs
@@ -3,11 +3,14 @@ module Test.MySolutions where
 import Prelude
 
 import ChapterExamples (Address, Amp(..), Person, Volt(..))
+import Data.Array (filter)
 import Data.Maybe (Maybe(..))
-import Data.Picture (Point, Shape(..), getCenter, origin)
+import Data.Picture (Bounds, Picture, Point, Shape(..), bounds, getCenter, origin, shapeBounds) as Pict
 import Math (pi)
 
 newtype Watt = Watt Number
+
+data ClippedShape = Shape Pict.Shape | Clipped Pict.Picture Pict.Point Number Number
 
 factorial :: Int -> Int
 factorial 0 = 1
@@ -32,35 +35,51 @@ fromSingleton :: forall a. a -> Array a -> a
 fromSingleton _ [value] = value
 fromSingleton def _ = def
 
-circleAtOrigin :: Shape
-circleAtOrigin = Circle origin 10.0
+circleAtOrigin :: Pict.Shape
+circleAtOrigin = Pict.Circle Pict.origin 10.0
 
-centerShape :: Shape -> Shape
-centerShape (Circle _ r) = Circle origin r
-centerShape (Rectangle _ w h) = Rectangle origin w h
-centerShape line@(Line start end) = Line (start - delta) (end - delta) where
-    delta :: Point
-    delta = getCenter line
-centerShape (Text _ text) = Text origin text
+centerShape :: Pict.Shape -> Pict.Shape
+centerShape (Pict.Circle _ r) = Pict.Circle Pict.origin r
+centerShape (Pict.Rectangle _ w h) = Pict.Rectangle Pict.origin w h
+centerShape line@(Pict.Line start end) = Pict.Line (start - delta) (end - delta) where
+    delta :: Pict.Point
+    delta = Pict.getCenter line
+centerShape (Pict.Text _ text) = Pict.Text Pict.origin text
 
-doubleScaleAndCenter :: Shape -> Shape
+doubleScaleAndCenter :: Pict.Shape -> Pict.Shape
 doubleScaleAndCenter = centerShape <<< scaleShape 2.0 where
-    scaleShape :: Number -> Shape -> Shape
-    scaleShape scale (Circle point radius)      = Circle point (radius * scale)
-    scaleShape scale (Rectangle point w h) = Rectangle point (w * scale) (h * scale)
-    scaleShape scale (Line start end)        = Line (scalePoint start) (scalePoint end) where
-        scalePoint :: Point -> Point
+    scaleShape :: Number -> Pict.Shape -> Pict.Shape
+    scaleShape scale (Pict.Circle point radius)      = Pict.Circle point (radius * scale)
+    scaleShape scale (Pict.Rectangle point w h) = Pict.Rectangle point (w * scale) (h * scale)
+    scaleShape scale (Pict.Line start end)        = Pict.Line (scalePoint start) (scalePoint end) where
+        scalePoint :: Pict.Point -> Pict.Point
         scalePoint { x, y } = { x: x * scale, y: y * scale }
-    scaleShape _ (Text point text)     = Text point text
+    scaleShape _ (Pict.Text point text)     = Pict.Text point text
 
 calculateWattage :: Amp -> Volt -> Watt
 calculateWattage (Amp amp) (Volt volt) = Watt (amp * volt)
 
-shapeText :: Shape -> Maybe String
-shapeText (Text _ text) = Just text
+shapeText :: Pict.Shape -> Maybe String
+shapeText (Pict.Text _ text) = Just text
 shapeText _ = Nothing
 
-area :: Shape -> Number
-area (Circle _ r) = pi * r * r
-area (Rectangle _ w h) = w * h
+area :: Pict.Shape -> Number
+area (Pict.Circle _ r) = pi * r * r
+area (Pict.Rectangle _ w h) = w * h
 area _ = 0.0
+
+contain :: Pict.Bounds -> Pict.Bounds -> Boolean
+contain bigB smlB | smlB.top >= bigB.top && smlB.right <= bigB.right && smlB.bottom <= bigB.bottom && smlB.left >= bigB.left = true
+contain _ _       | otherwise                                                                                                = false
+
+infix 9 contain as <$*>
+
+clip :: Pict.Picture -> Pict.Point -> Number -> Number -> Pict.Picture
+clip picture clipPoint clipW clipH = filter (buildBoundsFilter (Pict.shapeBounds (Pict.Rectangle clipPoint clipW clipH) )) picture where
+    buildBoundsFilter :: Pict.Bounds -> (Pict.Shape -> Boolean)
+    buildBoundsFilter rectBounds = \pictShape -> rectBounds <$*> Pict.shapeBounds pictShape
+
+{-  We need to clip that given picture with that clip fn and calculates the bounds of the new picture -}
+shapeBounds :: ClippedShape -> Pict.Bounds
+shapeBounds (Clipped picture point w h) = Pict.bounds $ clip picture point w h
+shapeBounds (Shape shape) = Pict.shapeBounds shape
