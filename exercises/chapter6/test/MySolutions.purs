12ca493664bd9499774572b1f843f0026f89e387
Walker <walker@praiseweb.com.br>
2021-10-04 11:09:49 -0300

    feat(ch6): add showPoint solution

--- exercises/chapter6/test/MySolutions.purs
+++ exercises/chapter6/test/MySolutions.purs
@@ -2,4 +2,11 @@ module Test.MySolutions where
 
 import Prelude
 
--- Note to reader: Add your solutions to this file
+newtype Point = Point
+  { x :: Number
+  , y :: Number
+  }
+
+instance showPoint :: Show Point where
+  show (Point point) = "(" <> show point.x <> ", " <> show point.y <> ")"
+
