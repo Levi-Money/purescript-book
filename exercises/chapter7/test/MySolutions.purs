3ca74845ab2e08692421990ef1b630eff72ba623
Walker <walker@praiseweb.com.br>
2021-11-26 10:50:33 -0300

    feat(ch7): add maybe lifted fns solution

--- exercises/chapter7/test/MySolutions.purs
+++ exercises/chapter7/test/MySolutions.purs
@@ -1,5 +1,23 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Maybe (Maybe)
+import Control.Apply (lift2)
 
--- Note to reader: Add your solutions to this file
+-- combineList :: forall f a. Applicative f => List (f a) -> f (List a)
+-- combineList (Cons x xs) = Cons <$> x <*> combineList xs
+-- combineList (Cons x xs) = (Cons <$> x) <*> (combineList xs)
+-- f ( xs -> Cons x xs ) <*> f xs =>
+-- f (Cons x xs)
+
+addMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+addMaybe = lift2 (+)
+
+subMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+subMaybe = lift2 (-)
+
+mulMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+mulMaybe = lift2 (*)
+
+divMaybe :: Maybe Int -> Maybe Int -> Maybe Int
+divMaybe = lift2 (/)
