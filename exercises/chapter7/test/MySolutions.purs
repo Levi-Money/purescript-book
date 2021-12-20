ed54008015fec5935ce51764cab083ac14e2e727
Walker <walker@praiseweb.com.br>
2021-12-20 09:39:45 -0300

    feat(ch7): add traversableTree solution

--- exercises/chapter7/test/MySolutions.purs
+++ exercises/chapter7/test/MySolutions.purs
@@ -1,16 +1,34 @@
-module Test.MySolutions where
+module Test.MySolutions
+  ( Tree(..)
+  , addApply
+  , addMaybe
+  , combineMaybe
+  , divApply
+  , divMaybe
+  , leaf
+  , mulApply
+  , mulMaybe
+  , nonEmptyRegex
+  , stateRegex
+  , subApply
+  , subMaybe
+  , validateAddressImproved
+  )
+  where
 
 import Prelude
 
 import Control.Apply (lift2)
 import Data.AddressBook (Address, address)
 import Data.AddressBook.Validation (Errors, matches)
+import Data.Foldable (class Foldable, foldMap, foldl, foldr)
 import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
 import Data.String.Regex (Regex)
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
+import Data.Traversable (class Traversable, sequence, traverse)
 import Data.Validation.Semigroup (V)
 
 addMaybe :: Maybe Int -> Maybe Int -> Maybe Int
@@ -37,7 +55,7 @@ mulApply = lift2 (*)
 divApply :: forall f. Apply f => f Int -> f Int -> f Int
 divApply = lift2 (/)
 
--- sequence :: forall a m. Applicative m => t (m a) -> m (t a)
+-- sequence :: forall a f. Applicative f => t (f a) -> f (t a)
 -- combineList :: forall f a. Applicative f => List (f a) -> f (List a)
 -- combineList (Cons x xs) = Cons <$> x <*> combineList xs
 
@@ -68,9 +86,30 @@ validateAddressImproved a =
 
 data Tree a = Leaf | Branch (Tree a) a (Tree a)
 
+leaf ∷ ∀ (a ∷ Type). a → Tree a
+leaf x = Branch Leaf x Leaf
+
 derive instance genericTree :: Generic (Tree a) _
 
 instance showTree :: Show a => Show (Tree a) where
-  show t = genericShow t
+    show x = genericShow x
 
 derive instance eqTree :: Eq a => Eq (Tree a)
+
+derive instance functorTree :: Functor Tree
+
+instance foldableTree :: Foldable Tree where
+    foldMap _ (Leaf) = mempty
+    foldMap f (Branch x y z) = foldMap f x <> f y <> foldMap f z
+
+    foldl _ xs (Leaf) = xs
+    foldl fn xs (Branch x y z) = foldl fn (fn (foldl fn xs x) y ) z
+
+    foldr _ xs (Leaf) = xs
+    foldr fn xs (Branch x y z) = foldr fn (fn y $ foldr fn xs z) x
+
+instance traversableTree :: Traversable Tree where
+    sequence   (Leaf)         = pure Leaf
+    sequence   (Branch x y z) = pure Branch <*> sequence x <*> y <*> sequence z
+    traverse _ (Leaf)         = pure Leaf
+    traverse f (Branch x y z) = pure Branch <*> traverse f x <*> f y <*> traverse f z
