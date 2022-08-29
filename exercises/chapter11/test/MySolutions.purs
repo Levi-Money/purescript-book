67c56d010ab491c636a9619352d0877f33b01755
Walker <walker@praiseweb.com.br>
2022-08-29 18:28:34 -0300

    feat(ch11): add testParens solution

--- exercises/chapter11/test/MySolutions.purs
+++ exercises/chapter11/test/MySolutions.purs
@@ -1,8 +1,27 @@
 module Test.MySolutions where
 
-import Prelude
+import Prelude (Unit, ($))
+import Control.Monad.State (State, execState, modify)
+import Data.String.CodeUnits (toCharArray)
+import Data.Foldable (traverse_)
 
 -- Note to reader : Add your solutions to this file
 
 -- because evalState returns the State return value, it returns unit
 -- because runState returns a Tuple having the last state and return value, it returns Tuple unit 21
+
+data Parens = Root | Nested Parens | Illegal
+
+testParens :: String -> Boolean
+testParens s = toBool $ execState (parse $ toCharArray s) Root where
+  toBool :: Parens -> Boolean
+  toBool Root = true
+  toBool _ = false
+  eval :: Char -> Parens -> Parens
+  eval '(' Root = Nested Root 
+  eval '(' (Nested p) = Nested (Nested p)
+  eval ')' (Nested p) = p
+  eval ')' Root = Illegal
+  eval _ n = n
+  parse :: Array Char -> State Parens Unit
+  parse = traverse_ \c -> modify \n -> eval c n
