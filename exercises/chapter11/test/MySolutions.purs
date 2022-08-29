383e942cd4c4ada95f6f7cb44b5c95f29daffe36
Walker <walker@praiseweb.com.br>
2022-08-29 16:19:50 -0300

    feat(ch11): add runState, evalState solution

--- exercises/chapter11/test/MySolutions.purs
+++ exercises/chapter11/test/MySolutions.purs
@@ -3,3 +3,6 @@ module Test.MySolutions where
 import Prelude
 
 -- Note to reader : Add your solutions to this file
+
+-- because evalState returns the State return value, it returns unit
+-- because runState returns a Tuple having the last state and return value, it returns Tuple unit 21
